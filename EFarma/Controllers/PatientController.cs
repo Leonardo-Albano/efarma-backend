e2d4a2e824ea493c6f0a748e712b0321c4b2d662
Leonardo Albano <leonardohenriquealbano@gmail.com>
2024-08-27 23:08:03 -0300

    First Commit!

---
+++ EFarma/Controllers/PatientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFarma.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+    }
+}
---
+++ EFarma/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿namespace EFarma.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Cpf { get; set; }
+        public DateOnly BirthDay { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Mail { get; set; } = string.Empty;
+        public string Observations { get; set; } = string.Empty;
+    }
+}
